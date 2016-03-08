afb494e85167a6554729bd40766303833b32ccf1
nmargolis <nmargolis89@gmail.com>
2016-03-07 16:36:39 -0800

    Add port

--- server.py
+++ server.py
@@ -577,4 +577,6 @@ def find_landlords_by_name(fname=None, lname=None):
 if __name__ == "__main__":
 
     connect_to_db(app)
-    app.run(debug=True)
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True, host="0.0.0.0", port=PORT)
